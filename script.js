8a357ce9a883bca532b939610e571b90eb903167
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-31 13:25:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+const inputE1=document.getElementById("newTodoInput");
+const buttonE1=document.getElementById("addTodoBtn");
+const olE1=document.getElementById("todoList");
+buttonE1.addEventListener('click',()=>{
+	let li=document.createElement("li");
+	
+	if(inputE1.value!==""){
+		li.textContent=inputE1.value;
+		olE1.appendChild(li);
+		inputE1.value="";
+	}
+	
+})
 
